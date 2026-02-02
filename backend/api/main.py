e2cd7229bf886cdb550be9422922f732d5c3bc43
Isaac Antwi <antwikisaac@gmail.com>
2026-02-02 08:21:31 +0000

    Add port to the main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -34,3 +34,10 @@ async def remove_bg(file: UploadFile = File(...)):
         content=buffer.getvalue(),
         media_type="image/png"
     )
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run sets PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
